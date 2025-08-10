010a859feaff9b2d4de8c775a490c5a28166fcbf
Rahul Kumar Yadav <ry573870@gmail.com>
2025-08-11 01:14:04 +0530

    products rendered on ui

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const LatestCollection = () => {
+    const {products} = useContext(ShopContext);  
+    
+    const[latestProduct, setLatestProduct] = useState([]);
+
+    useEffect(()=>{
+        setLatestProduct(products.slice(0,10));
+    },[])
+  return (
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'Latest'} text2={'Collection'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+            </p>
+        </div>
+
+        {/* Rendering products */}
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {
+                latestProduct.map((item,index)=>{
+                    return(
+                        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+                    );
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default LatestCollection
