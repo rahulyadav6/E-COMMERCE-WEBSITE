6ae4d53adbe27a9ad277bdc1f557bded607d80cf
Rahul Kumar Yadav <ry573870@gmail.com>
2025-08-18 08:38:32 +0530

    cart total price calculation done

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -58,12 +58,30 @@ const ShopContextProvider = (props)=>{
         setCartItems(cartData);
     }
 
+    const getCartAmount = ()=>{
+        let totalAmount = 0;
+        for(const items in cartItems){
+            let itemInfo = products.find((product)=> product._id === items);
+            for(const item in cartItems[items]){
+                try {
+                    if(cartItems[items][item] > 0 ){
+                        totalAmount += itemInfo.price * cartItems[items][item];  
+                    }
+                } catch (error) {
+                    toast.error(error);
+                }
+            }
+        }
+        return totalAmount;
+    }
+
 
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCartCount, updateQuantity,
+        getCartAmount,
     }
 
     return(
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/frontend_assets/assets';
+import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
 
@@ -55,6 +56,11 @@ const Cart = () => {
         }
       </div>
 
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal/>
+        </div>
+      </div>
 
     </div>
   )
