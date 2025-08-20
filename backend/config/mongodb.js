92cb496953a7faea28fc0b8a614ecc48fd81736d
Rahul Kumar Yadav <ry573870@gmail.com>
2025-08-20 06:59:57 +0530

    basic setup || product model created

---
+++ backend/config/mongodb.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=>{
+    
+    try{
+        mongoose.connection.on('connected', ()=>{
+            console.log("DB connected")
+        })
+        await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`);
+    }catch(error){
+        console.error("Database connection failed:", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ backend/models/productModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: Array, required: true },
+    category: { type: string, required: true },
+    subCategory: { type: string, required: true },
+    sizes: { type: Array, required: true },
+    bestSeller: { type: Boolean},
+    date: { type: Number, required: true },
+})
+
+const productModel = mongoose.models.product || mongoose.model("product", productSchema);
+
+export default productModel;
