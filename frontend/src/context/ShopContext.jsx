5f376758ac0586aebe1bd40d78ba0ceaa6f6076e
Rahul Kumar Yadav <ry573870@gmail.com>
2025-08-25 02:11:48 +0530

    added backend url to .env file of frontend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,6 +10,7 @@ const ShopContextProvider = (props)=>{
 
     const currency = '$';
     const delivery_fee = 10;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
@@ -83,7 +84,7 @@ const ShopContextProvider = (props)=>{
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCartCount, updateQuantity,
-        getCartAmount, navigate,
+        getCartAmount, navigate, backendUrl
     }
 
     return(
