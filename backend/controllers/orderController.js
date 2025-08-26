d88f740296471d6df0166af8b7f20d83bb168ef5
Rahul Kumar Yadav <ry573870@gmail.com>
2025-08-27 01:30:43 +0530

    placeOrder using COD backend api complete

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,57 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel";
+
+// placing order using COD method
+const placeOrder = async(req,res)=>{
+    try {
+        const { userId, items, amount, address } = req.body;
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod: "COD",
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+
+        await userModel.findByIdAndUpdate(userId,{cartData:{}});
+
+        res.json({success:true, message:"Order Placed"});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message});
+    }
+}
+
+// placing order using Stripe method
+const placeOrderStripe = async(req,res)=>{
+    
+}
+
+// placing order using placeOrderRazorpay method
+const placeOrderRazorpay = async(req,res)=>{
+    
+}
+
+
+// All orders data for admin panel
+const allOrders = async(req,res)=>{
+    
+}
+
+// User oder data for frontend
+const userOrders = async(req,res)=>{
+    
+}
+
+// update order status from admin panel
+const updateStatus = async(req,res)=>{
+    
+}
+
+export { placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, userOrders, updateStatus }
