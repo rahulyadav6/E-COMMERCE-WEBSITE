d9607b0cac58fc5440d991f99c389d7057389b30
Rahul Kumar Yadav <ry573870@gmail.com>
2025-08-27 02:47:59 +0530

    show all orders in orders page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -49,7 +49,7 @@ const userOrders = async(req,res)=>{
     try {
         const { userId } = req.body;
         const orders = await orderModel.find({ userId });
-        res.json({ succes:true, orders });
+        res.json({ success:true, orders });
     } catch (error) {
         console.log(error);
         res.json({success:false, message:error.message}); 
