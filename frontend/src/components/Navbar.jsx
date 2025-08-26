a92f9009f2555c6ae1011822ee08c14249ab8cef
Rahul Kumar Yadav <ry573870@gmail.com>
2025-08-26 21:53:05 +0530

    log out functionality complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { ShopContext } from '../context/ShopContext';
 const Navbar = () => {
 
     const[visible, setVisible] = useState(false);
-    const { setShowSearch, getCartCount, navigate, setToken, setCartItems } = useContext(ShopContext);
+    const { setShowSearch, getCartCount, navigate, token, setToken, setCartItems } = useContext(ShopContext);
     const logout = ()=>{
         localStorage.removeItem('token');
         setToken('');
@@ -39,14 +39,16 @@ const Navbar = () => {
         <div className='flex items-center gap-6'>
             <img onClick={()=>(setShowSearch(true))} src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
             <div className='group relative'>
-                <Link to={'/login'}><img src={assets.profile_icon} className='w-5 cursor-pointer'/> </Link>
+                <img onClick={()=>token ? null: navigate('/login') } src={assets.profile_icon} className='w-5 cursor-pointer'/>
+                {/* dropdown menu */}
+                {token && 
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded-md'>
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
-                        <p className='cursor-pointer hover:text-black'>Orders</p>
+                        <p onClick={()=> navigate('/Orders')} className='cursor-pointer hover:text-black'>Orders</p>
                         <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
                     </div>
-                </div>
+                </div>}
             </div>
             <Link to='/cart' className='relative'>
                 <img src={assets.cart_icon} className='w-5 min-w-5'/>
