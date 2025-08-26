6a35d1fe4155cd0de7f8d47b1e8f93f385648bf7
Rahul Kumar Yadav <ry573870@gmail.com>
2025-08-26 22:16:13 +0530

    get user cart api complete

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -50,7 +50,15 @@ const updateCart = async(req,res)=>{
 
 // get user cart data
 const getUserCart = async(req,res)=>{
-
+    try {
+        const { userId } = req.body;
+        const userData = await userModel.findById(userId);
+        let cartData = await userData.cartData;
+        res.json({success:true, cartData})
+    } catch (error) {
+        console.log(error);
+        res.json({ success:false, message: error.message });
+    }
 }
 
 export { addToCart, updateCart, getUserCart }
