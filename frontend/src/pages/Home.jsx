9a944288fa57eb538c68abaadc5aa165a95024e2
Rahul Kumar Yadav <ry573870@gmail.com>
2025-09-14 12:33:50 +0530

    error handled

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
-import Newsletterbox from '../components/NewsletterBox'
+import Newsletterbox from '../components/Newsletterbox'
 
 const Home = () => {
   return (
